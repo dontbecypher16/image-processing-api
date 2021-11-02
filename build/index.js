f6bb4b3ffcfdd231ce25dce14fd9b65c054949fc
Richard Dixon <rvictordixon@gmail.com>
2021-11-02 04:39:57 -0400

    Still doing config.

--- build/index.js
+++ build/index.js
@@ -1,13 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Just a test')
-})
-
+app.get("/", (req, res) => {
+  res.send("Just a test");
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on localhost${port}`)
-})
-
+  console.log(`Server listening on localhost${port}`);
+});
