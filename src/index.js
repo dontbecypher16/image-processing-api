0d05b89b5cd39f4a78fb5d6efc799e6d50f74fe1
Richard Dixon <rvictordixon@gmail.com>
2021-11-06 17:57:22 -0400

    renamed some files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const axios = require('axios')
-const newImages = require('../build/sharp-logic')
-
+const cors = require("cors");
+const axios = require("axios");
+const newImages = require("../build/sharp-logic");
 
 // app.use(function (req, res, next) {
 //     res.setHeader(
@@ -13,22 +13,21 @@ const newImages = require('../build/sharp-logic')
 //     next();
 //   });
 
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}))
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/data", cors());
 
 app.get("/", (req, res, next) => {
   res.send("Just a test");
-  next()
+  next();
 });
 
 app.get("/data", (req, res) => {
-    //res.type('application/json')
-    //newData(([key, value]) => `${key}: ${value}`)
-    res.send(`${newImages}`)
+  //res.type('application/json')
+  //newData(([key, value]) => `${key}: ${value}`)
+  res.json(`${newImages}`);
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server listening on localhost${port}`);
