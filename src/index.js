fdee3ea1620bc441a27986392249d56a229db874
Richard Dixon <rvictordixon@gmail.com>
2021-11-12 23:40:42 -0500

    Get dynamic values, working on rendering.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require("express")
 const app = express()
 const port = 3000
-const axios = require("axios")
 const imgFile  = require("./sharp-logic")
-
 
 app.use(express.static(__dirname + '/public/images'))
 app.use(express.json())
@@ -22,38 +20,12 @@ app.get('/', (req, res) => {
   if (heightStr) {
     height = parseInt(heightStr)
   }
-  res.type('image/jpg')
-    imgFile( filename, width, height).pipe(res)
+  res.type('image/jpeg')
+    imgFile( filename, width, height)//.pipe(res)
     //console.log(imgFile)
 })
 
 
-// axios.get('/pics?imgfolder')
-//   .then(res => {
-//     console.log(res.data);
-//   });
-
-// app.get("/data", (req, res) => {
-//   //res.type('application/json')
-//   //newData(([key, value]) => `${key}: ${value}`)
-//   console.log(imgfolder)
-//   let picname = req.query.picname
-//   console.log(`imgfolder.${picname}`)
-//   console.log([picname])
-
-
-//   res.send(picname)
-//   // for(let i = 0; i < imgfolder; i++){
-//   //   //if(req.query.)
-//   //   let filename = imgfolder[i] 
-//   //   //console.log(filename)
-//   //   return filename
-//   // }
-//   // res.render('data', {
-//   //   if(filename[key] === )
-//   // })
-
-// })
 
 app.listen(port, () => {
   console.log(`Server listening on localhost:${port}`)
--- src/sharp-logic.js
+++ src/sharp-logic.js
@@ -1,97 +1,17 @@
 const sharp = require("sharp");
-const fs = require("fs")
 
 
 
-module.exports = async function imgFile(path, filename, width, height) {
+module.exports = async function imgFile(filename, width, height) {
   try {
+    const transform = await sharp(`public/images/${filename}.jpg`)
+      .resize(width, height)
+      .toFile(`public/new-images/${filename}.jpg`);
 
-    path =  `/public/images/${filename}`
-   const readStream = fs.createReadStream(path)
-   let transform =  sharp()
- 
-   if (filename && (width || height)) {
-     transform = await transform.resize(width, height).toFile(`/public/new-images/${filename}`)
-   }
- 
-   //console.log(transform)
-   return readStream.pipe(transform)
-
-  } catch(err){
-
-    console.log(err)
-
+    return transform
+  } catch (err) {
+    console.log(`An error has occurred: ${err}`);
   }
 }
-
-
-// async function fjord() {
-//   try {
-//     const oldfjord = await new sharp("public/images/fjord.jpg")
-//       .resize({ width: 200, height: 300 })
-//       .toFile(`public/new-images/new-fjord.jpg`);
-
-//     return oldfjord
-//   } catch (err) {
-//     console.log(`An error has occurred: ${err}`);
-//   }
-// }
-
-// async function encenadaport() {
-//   try {
-//     const oldEncena = await new sharp("public/images/encenadaport.jpg")
-//       .resize({ width: 200, height: 300 })
-//       .toFile("public/new-images/new-encenadaport.jpg");
-
-//     return oldEncena
-//   } catch (err) {
-//     console.log(`An error has occurred: ${err}`);
-//   }
-// }
-
-// async function icelandwaterfall() {
-//   try {
-//     const oldIce = await new sharp("public/images/icelandwaterfall.jpg")
-//       .resize({ width: 200, height: 300 })
-//       .toFile("public/new-images/new-icelandwaterfall.jpg");
-
-//     return oldIce
-//   } catch (err) {
-//     console.log(`An error has occurred: ${err}`);
-//   }
-// }
-
-// async function palmtunnel() {
-//   try {
-//     const oldPalm = await new sharp("public/images/palmtunnel.jpg")
-//       .resize({ width: 200, height: 300 })
-//       .toFile("public/new-images/new-palmtunnel.jpg");
-
-//     return oldPalm
-//   } catch (err) {
-//     console.log(`An error has occurred: ${err}`);
-//   }
-// }
-
-// async function santamonica() {
-//   try {
-//     const oldSanta = await new sharp("public/images/santamonica.jpg")
-//       .resize({ width: 200, height: 300 })
-//       .toFile("public/new-images/new-santamonica.jpg");
-
-//     return oldSanta
-//   } catch (err) {
-//     console.log(`An error has occurred: ${err}`);
-//   }
-// }
-
-// // module.exports = { fjord, encenadaport, santamonica,
-// //    icelandwaterfall, palmtunnel }
-
-//    module.exports = fjord()
-//    module.exports = encenadaport()
-//    module.exports = santamonica()
-//    module.exports = icelandwaterfall()
-//    module.exports = palmtunnel()
 
 
