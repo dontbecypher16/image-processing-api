8d92fa1e2bb97a7107806c619163489147f0417d
Richard Dixon <rvictordixon@gmail.com>
2021-11-13 02:37:56 -0500

    Finished images, add typescript.

--- src/sharp-logic.js
+++ src/sharp-logic.js
@@ -1,11 +1,12 @@
 const sharp = require("sharp");
 
 
-
-module.exports = async function imgFile(filename, width, height) {
+module.exports = function imgFile(filename, width, height) {
   try {
-    const transform = await sharp(`public/images/${filename}.jpg`)
+    const transform = sharp(`public/images/${filename}.jpg`)
       .resize(width, height)
+    transform
+      .clone()
       .toFile(`public/new-images/${filename}.jpg`);
 
     return transform
