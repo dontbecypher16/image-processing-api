412199a65ca083f2ad60d531e0a72420f0391c97
Richard Dixon <rvictordixon@gmail.com>
2021-11-12 20:52:40 -0500

    having issues with sharp module.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,60 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-//const axios = require("axios");
-const imgfolder  = require("./sharp-logic");
+const express = require("express")
+const app = express()
+const port = 3000
+const axios = require("axios")
+const imgFile  = require("./sharp-logic")
 
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public/images'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 
-app.get("/", (req, res, next) => {
-  res.send("Just a test");
-  next();
-});
+app.get('/', (req, res) => {
+  const widthStr = req.query.width
+  const heightStr = req.query.height
+  const filename = req.query.filename
 
-app.get("/data", (req, res) => {
-  //res.type('application/json')
-  //newData(([key, value]) => `${key}: ${value}`)
-  console.log(imgfolder)
-  let picname = req.query.picname
-  console.log(`imgfolder.fjord.${picname}`)
+  let width, height
+  if (widthStr) {
+    width = parseInt(widthStr)
+  }
+  if (heightStr) {
+    height = parseInt(heightStr)
+  }
+  res.type('image/jpg')
+    imgFile( filename, width, height).pipe(res)
+    //console.log(imgFile)
+})
 
 
-  res.sendFile(imgfolder)
-  // for(let i = 0; i < imgfolder; i++){
-  //   //if(req.query.)
-  //   let filename = imgfolder[i] 
-  //   //console.log(filename)
-  //   return filename
-  // }
-  // res.render('data', {
-  //   if(filename[key] === )
-  // })
+// axios.get('/pics?imgfolder')
+//   .then(res => {
+//     console.log(res.data);
+//   });
 
-});
+// app.get("/data", (req, res) => {
+//   //res.type('application/json')
+//   //newData(([key, value]) => `${key}: ${value}`)
+//   console.log(imgfolder)
+//   let picname = req.query.picname
+//   console.log(`imgfolder.${picname}`)
+//   console.log([picname])
+
+
+//   res.send(picname)
+//   // for(let i = 0; i < imgfolder; i++){
+//   //   //if(req.query.)
+//   //   let filename = imgfolder[i] 
+//   //   //console.log(filename)
+//   //   return filename
+//   // }
+//   // res.render('data', {
+//   //   if(filename[key] === )
+//   // })
+
+// })
 
 app.listen(port, () => {
-  console.log(`Server listening on localhost${port}`);
-});
+  console.log(`Server listening on localhost:${port}`)
+})
--- src/sharp-logic.js
+++ src/sharp-logic.js
@@ -1,70 +1,97 @@
 const sharp = require("sharp");
-//const router = require('express').Router()
-//const fs = require("fs")
+const fs = require("fs")
 
 
 
-
-async function fjord() {
+module.exports = async function imgFile(path, filename, width, height) {
   try {
-    const oldfjord = await new sharp("public/images/fjord.jpg")
-      .resize({ width: 200, height: 300 })
-      .toFile(`public/new-images/fjord.jpg`);
 
-    return oldfjord
-  } catch (err) {
-    console.log(`An error has occurred: ${err}`);
+    path =  `/public/images/${filename}`
+   const readStream = fs.createReadStream(path)
+   let transform =  sharp()
+ 
+   if (filename && (width || height)) {
+     transform = await transform.resize(width, height).toFile(`/public/new-images/${filename}`)
+   }
+ 
+   //console.log(transform)
+   return readStream.pipe(transform)
+
+  } catch(err){
+
+    console.log(err)
+
   }
 }
 
-async function encenadaport() {
-  try {
-    const oldEncena = await new sharp("public/images/encenadaport.jpg")
-      .resize({ width: 200, height: 300 })
-      .toFile("public/new-images/encenadaport.jpg");
 
-    return oldEncena
-  } catch (err) {
-    console.log(`An error has occurred: ${err}`);
-  }
-}
+// async function fjord() {
+//   try {
+//     const oldfjord = await new sharp("public/images/fjord.jpg")
+//       .resize({ width: 200, height: 300 })
+//       .toFile(`public/new-images/new-fjord.jpg`);
 
-async function icelandwaterfall() {
-  try {
-    const oldIce = await new sharp("public/images/icelandwaterfall.jpg")
-      .resize({ width: 200, height: 300 })
-      .toFile("public/new-images/new-icelandwaterfall.jpg");
+//     return oldfjord
+//   } catch (err) {
+//     console.log(`An error has occurred: ${err}`);
+//   }
+// }
 
-    return oldIce
-  } catch (err) {
-    console.log(`An error has occurred: ${err}`);
-  }
-}
+// async function encenadaport() {
+//   try {
+//     const oldEncena = await new sharp("public/images/encenadaport.jpg")
+//       .resize({ width: 200, height: 300 })
+//       .toFile("public/new-images/new-encenadaport.jpg");
 
-async function palmtunnel() {
-  try {
-    const oldPalm = await new sharp("public/images/palmtunnel.jpg")
-      .resize({ width: 200, height: 300 })
-      .toFile("public/new-images/new-palmtunnel.jpg");
+//     return oldEncena
+//   } catch (err) {
+//     console.log(`An error has occurred: ${err}`);
+//   }
+// }
 
-    return oldPalm
-  } catch (err) {
-    console.log(`An error has occurred: ${err}`);
-  }
-}
+// async function icelandwaterfall() {
+//   try {
+//     const oldIce = await new sharp("public/images/icelandwaterfall.jpg")
+//       .resize({ width: 200, height: 300 })
+//       .toFile("public/new-images/new-icelandwaterfall.jpg");
 
-async function santamonica() {
-  try {
-    const oldSanta = await new sharp("public/images/santamonica.jpg")
-      .resize({ width: 200, height: 300 })
-      .toFile("public/new-images/new-santamonica.jpg");
+//     return oldIce
+//   } catch (err) {
+//     console.log(`An error has occurred: ${err}`);
+//   }
+// }
 
-    return oldSanta
-  } catch (err) {
-    console.log(`An error has occurred: ${err}`);
-  }
-}
+// async function palmtunnel() {
+//   try {
+//     const oldPalm = await new sharp("public/images/palmtunnel.jpg")
+//       .resize({ width: 200, height: 300 })
+//       .toFile("public/new-images/new-palmtunnel.jpg");
 
-module.exports = { fjord, encenadaport, santamonica,
-   icelandwaterfall, palmtunnel }
+//     return oldPalm
+//   } catch (err) {
+//     console.log(`An error has occurred: ${err}`);
+//   }
+// }
+
+// async function santamonica() {
+//   try {
+//     const oldSanta = await new sharp("public/images/santamonica.jpg")
+//       .resize({ width: 200, height: 300 })
+//       .toFile("public/new-images/new-santamonica.jpg");
+
+//     return oldSanta
+//   } catch (err) {
+//     console.log(`An error has occurred: ${err}`);
+//   }
+// }
+
+// // module.exports = { fjord, encenadaport, santamonica,
+// //    icelandwaterfall, palmtunnel }
+
+//    module.exports = fjord()
+//    module.exports = encenadaport()
+//    module.exports = santamonica()
+//    module.exports = icelandwaterfall()
+//    module.exports = palmtunnel()
+
 
